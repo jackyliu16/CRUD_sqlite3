371e94fbddfc87a2ca81f58d54da03ee36e18eff
jackyLiu <18922251299@163.com>
2022-10-03 01:03:05 +0800

    Nothing:(simple test in sqlite.rs)
    	1. trying to make a execute function which get sql and return
    	   it's output in sqlite for egui programe to point out.
    	2. now we are faceing in a question how we change the output of
    	   sqlite3 into rust and to egui.
    		1. first trying to using vec<vec<_>> to saving all data,
    		   thus `determine the type before compiling`, **failure**.
    		2. trying to find a ways to using stmt.cloumns() to get
    		   column information such as column name and type, but
    		   seem not work due to `all type should be determine
    		   before compiling`, **failure** but thinking about
    		   using a kind of lazy_static or static_init to init
    		   the struct. *(but seem type couldn't init at this
    		   time)*
    		3. also trying to find ways in generic.
    	修改：     Cargo.toml
    	修改：     src/lib/sqlite.rs

--- simple_sqlite_operating/src/lib/sqlite.rs
+++ simple_sqlite_operating/src/lib/sqlite.rs
@@ -4,7 +4,7 @@ use rusqlite::{Connection, Result};
 #[cfg(test)]
 mod tests {
     use std::env::current_dir;
-    use rusqlite::{Connection, Result};
+    use rusqlite::{Connection, Result, Column};
     use crate::lib::fd::{self, *};
 
     #[test]
@@ -28,4 +28,67 @@ mod tests {
 
         // let conn = Connection::open()
     }
+
+    use rusqlite::params;
+    #[test]
+    pub fn test_for_connection_and_execute() {
+        let mut cwd = current_dir().unwrap();
+        // let file = get_file_under_path(&cwd);
+        fd::test::print_everything(&cwd);
+        println!("=========================");
+        
+        move_to_direntry(&mut cwd, "school.db".to_string());
+        print_pwd(&cwd);
+
+        // TODO 这个地方需要错误处理，但是我目前掌握相对来说不是很好
+        let conn = Connection::open(&cwd).unwrap();
+        match conn.path() {
+            Some(conn) => println!("connection successed"),
+            None => println!("connection failure"),
+        }
+        //.head on
+        //.mode column 
+        let stmt = conn.prepare("SELECT * FROM ADVISOR").unwrap();
+        let col_num = stmt.column_count();
+        let cols = stmt.columns();
+        
+        // let mut table = Vec::new();
+
+        // println!("col: {}", col_num);
+
+        let mut stmt = conn.prepare("SELECT * FROM ADVISOR").unwrap();
+        let mut rows = stmt.query([]).unwrap();
+
+        // let mut table:Vec<_> = Vec::new();
+        // for j in 0..col_num {
+        //     let tmp:Vec<_> = Vec::new();
+        //     while let Some(row) = rows.next().unwrap() {
+        //         tmp.push(row.get(j).unwrap());
+        //     }
+        //     println!("{:?}", tmp);
+        //     table.push(tmp);
+        // }
+        // let mut vec_of_object = Vec::new();
+        while let Some(row) = rows.next().unwrap() {
+            // let mut tmp: Vec<String> = Vec::new();
+            for i in 0..col_num {
+                let a: String = row.get(i).unwrap();
+                print!("{:?}", a);
+            } 
+            println!("");
+            // println!("tmp: {:?}", tmp
+            // vec_of_object.push(tmp);
+        }
+    
+        // for row in vec_of_object {
+        //     println!("{:?}", row);
+        // }
+
+        // let conn = Connection::open()
+    }
+
+    pub fn col_type_check(cols: &Vec<Column<'_>>, idx: usize) -> String {
+        cols[idx].decl_type().unwrap().to_string()
+    }
+
 }
